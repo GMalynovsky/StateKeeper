a184279481b3620cb3295280f031ed6b2b3bcd09
gmaly-definely <glib@definely.com>
2025-04-20 20:19:40 +0300

    Add project files.

---
+++ TokenKeeper.Tests/TokenStateKeeperTests.cs
@@ -0,0 +1,232 @@
+using System.Diagnostics;
+
+namespace TokenKeeper.Tests;
+
+[TestClass]
+public class TokenStateKeeperTests
+{
+    private static TokenStateKeeper Create() => new();
+
+    [TestMethod]
+    public void Seed_DuplicateHash_ReturnsDuplicate()
+    {
+        var sut = Create();
+        Assert.AreEqual(TokenOpResult.Success, sut.Seed(1, "A"));
+        Assert.AreEqual(TokenOpResult.DuplicateHash, sut.Seed(1, "B"));
+    }
+
+    [TestMethod]
+    public void StageInsert_UncommittedDiffContainsNewToken()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Stage(null, 2, "B");
+        var diff = sut.GetUncommittedDiff().Single();
+        Assert.IsNull(diff.LeftHash);
+        Assert.AreEqual(2L, diff.RightHash);
+    }
+
+    [TestMethod]
+    public void StageDelete_UnknownHash_ReturnsUnknown()
+    {
+        var sut = Create();
+        var res = sut.Stage(99, null, "X");
+        Assert.AreEqual(TokenOpResult.UnknownHash, res);
+    }
+
+    [TestMethod]
+    public void StageModify_HashCollision_ReturnsCollision()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Seed(2, "B");
+        var res = sut.Stage(1, 2, "A->B");
+        Assert.AreEqual(TokenOpResult.Collision, res);
+    }
+
+    [TestMethod]
+    public void StageSameTokenTwice_ReturnsAlreadyStaged()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Stage(1, 3, "A*");
+        var res = sut.Stage(1, 4, "A**");
+        Assert.AreEqual(TokenOpResult.AlreadyStaged, res);
+    }
+
+    [TestMethod]
+    public void Commit_MovesDiffToCommitted()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Stage(1, 2, "A*");
+        sut.Commit();
+        Assert.AreEqual(1, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void FullDiff_FallsBackToInitial()
+    {
+        var sut = Create();
+        sut.Seed(5, "Z");
+        sut.Stage(5, null, "");
+        sut.Commit();
+        var diff = sut.GetFullDiff().Single();
+        Assert.AreEqual(5L, diff.LeftHash);
+        Assert.IsNull(diff.RightHash);
+    }
+
+    [TestMethod]
+    public void Discard_ClearsUncommitted()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Stage(1, 2, "A*");
+        sut.Discard();
+        Assert.AreEqual(0, sut.GetUncommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void DeleteThenReinsert_SameHashAllowed()
+    {
+        var sut = Create();
+        sut.Seed(10, "X");
+        sut.Stage(10, null, "");
+        sut.Commit();
+        var res = sut.Stage(null, 10, "X2");
+        Assert.AreEqual(TokenOpResult.Success, res);
+    }
+
+    [TestMethod]
+    public void Stage_BothHashesNull_ReturnsInvalid()
+    {
+        var sut = Create();
+        var res = sut.Stage(null, null, "Y");
+        Assert.AreEqual(TokenOpResult.InvalidInput, res);
+    }
+
+    [TestMethod]
+    public void TryGetSnapshot_Unknown_ReturnsFalse()
+    {
+        var sut = Create();
+        Assert.IsFalse(sut.TryGetSnapshot(123, out _));
+    }
+
+    [TestMethod]
+    public void Commit_WithEmptyStaging_NoDiff()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Commit();
+        Assert.AreEqual(0, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void MixedBatch_ModifyDeleteInsert_DiffCountThree()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Seed(2, "B");
+        sut.Seed(3, "C");
+        sut.Stage(1, 11, "A*");
+        sut.Stage(2, null, "");
+        sut.Stage(null, 12, "D");
+        sut.Commit();
+        Assert.AreEqual(3, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void ParallelStage_1000Tokens_DoesNotThrow()
+    {
+        var sut = Create();
+        const int n = 1000;
+        for (var i = 0; i < n; i++) sut.Seed(i, $"V{i}");
+        Parallel.For(0, n, i => sut.Stage(i, i + n, $"V{i}*"));
+        sut.Commit();
+        Assert.AreEqual(n, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void Stress_Commit100k_UnderTwoSeconds()
+    {
+        var sut = Create();
+        const int count = 100_000;
+        for (var i = 0; i < count; i++) sut.Seed(i, $"V{i}");
+        for (var i = 0; i < count; i++) sut.Stage(i, i + count, $"V{i}*");
+        var sw = Stopwatch.StartNew();
+        sut.Commit();
+        sw.Stop();
+        Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(2));
+        Assert.AreEqual(count, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void InsertThenCommit_ShowsInsertInCommittedDiff()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Stage(null, 3, "C");
+        sut.Commit();
+        var diff = sut.GetCommittedDiff().Single(d => d.RightHash == 3);
+        Assert.IsNull(diff.LeftHash);
+        Assert.AreEqual(3L, diff.RightHash);
+    }
+
+    [TestMethod]
+    public void UncommittedDiff_InsertModifyDelete_AllReported()
+    {
+        var sut = Create();
+        sut.Seed(1, "A");
+        sut.Seed(2, "B");
+        sut.Stage(1, 10, "A*");
+        sut.Stage(2, null, "");
+        sut.Stage(null, 11, "C");
+        Assert.AreEqual(3, sut.GetUncommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void ParallelStage_10000Tokens_DiffMatches()
+    {
+        var sut = Create();
+        const int n = 10_000;
+        for (var i = 0; i < n; i++) sut.Seed(i, $"V{i}");
+        Parallel.For(0, n, i => sut.Stage(i, i + n, $"V{i}*"));
+        sut.Commit();
+        Assert.AreEqual(n, sut.GetCommittedDiff().Count());
+    }
+
+    [TestMethod]
+    public void Delete_RemovesHashMapping_AllowsReSeed()
+    {
+        var sut = Create();
+        sut.Seed(42, "X");
+        sut.Stage(42, null, "");
+        sut.Commit();
+        var res = sut.Seed(42, "Y");
+        Assert.AreEqual(TokenOpResult.Success, res);
+    }
+
+    [TestMethod]
+    public void Concurrent_StageAndCommit_MultiThreadIntegrity()
+    {
+        var sut = Create();
+        const int threads = 8;
+        const int perThread = 5000;
+        var seedCounter = 0;
+
+        Parallel.For(0, threads, _ =>
+        {
+            var start = Interlocked.Add(ref seedCounter, perThread) - perThread;
+            for (var i = 0; i < perThread; i++) sut.Seed(start + i, "I");
+        });
+
+        Parallel.For(0, threads, t =>
+        {
+            var offset = t * perThread;
+            for (var i = 0; i < perThread; i++) sut.Stage(offset + i, offset + i + 50_000, "M");
+        });
+
+        sut.Commit();
+        Assert.AreEqual(threads * perThread, sut.GetCommittedDiff().Count());
+    }
+}
---
+++ TokenKeeper/TokenStateKeeper.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TokenKeeper
+{
+    internal struct TokenState
+    {
+        public long? Initial;
+        public long? Previous;
+        public long? Current;
+    }
+
+    public readonly struct TokenSnapshot<T>
+    {
+        public TokenSnapshot(long? initialHash, long? previousHash, long? currentHash, T initialValue, T previousValue, T currentValue)
+        {
+            InitialHash = initialHash;
+            PreviousHash = previousHash;
+            CurrentHash = currentHash;
+            InitialValue = initialValue;
+            PreviousValue = previousValue;
+            CurrentValue = currentValue;
+        }
+        public long? InitialHash { get; }
+        public long? PreviousHash { get; }
+        public long? CurrentHash { get; }
+        public T InitialValue { get; }
+        public T PreviousValue { get; }
+        public T CurrentValue { get; }
+    }
+
+    public readonly struct TokenDiff<T>
+    {
+        public TokenDiff(long? leftHash, long? rightHash, T leftValue, T rightValue)
+        {
+            LeftHash = leftHash;
+            RightHash = rightHash;
+            LeftValue = leftValue;
+            RightValue = rightValue;
+        }
+        public long? LeftHash { get; }
+        public long? RightHash { get; }
+        public T LeftValue { get; }
+        public T RightValue { get; }
+    }
+
+    public enum TokenOpResult
+    {
+        Success,
+        DuplicateHash,
+        UnknownHash,
+        Collision,
+        AlreadyStaged,
+        InvalidInput
+    }
+
+    public interface ITokenInitializer<T>
+    {
+        TokenOpResult Seed(long hash, T value);
+    }
+
+    public interface ITokenMutator<T>
+    {
+        TokenOpResult Stage(long? oldHash, long? newHash, T value);
+        void Commit();
+        void Discard();
+    }
+
+    public interface ITokenReader<T>
+    {
+        bool TryGetSnapshot(long hash, out TokenSnapshot<T> snapshot);
+        IEnumerable<TokenDiff<T>> GetCommittedDiff();
+        IEnumerable<TokenDiff<T>> GetUncommittedDiff();
+        IEnumerable<TokenDiff<T>> GetFullDiff();
+    }
+
+    internal abstract class StateKeeper<T> : ITokenInitializer<T>, ITokenMutator<T>, ITokenReader<T>
+    {
+        private readonly Dictionary<Guid, TokenState> _states = new Dictionary<Guid, TokenState>();
+        private readonly Dictionary<Guid, long?> _staging = new Dictionary<Guid, long?>();
+        private readonly Dictionary<long, Guid> _hash2Id = new Dictionary<long, Guid>();
+        private readonly Dictionary<long, T> _pool = new Dictionary<long, T>();
+        private readonly object _sync = new object();
+
+        public TokenOpResult Seed(long hash, T value)
+        {
+            lock (_sync)
+            {
+                if (_hash2Id.ContainsKey(hash)) return TokenOpResult.DuplicateHash;
+                var id = Guid.NewGuid();
+                _hash2Id[hash] = id;
+                _pool[hash] = value;
+                _states[id] = new TokenState { Initial = hash, Previous = hash, Current = hash };
+                return TokenOpResult.Success;
+            }
+        }
+
+        public TokenOpResult Stage(long? oldHash, long? newHash, T value)
+        {
+            lock (_sync)
+            {
+                if (oldHash.HasValue && !newHash.HasValue) return StageDelete(oldHash.Value);
+                if (!oldHash.HasValue && newHash.HasValue) return StageInsert(newHash.Value, value);
+                if (oldHash.HasValue && newHash.HasValue) return StageModify(oldHash.Value, newHash.Value, value);
+                return TokenOpResult.InvalidInput;
+            }
+        }
+
+        public void Commit()
+        {
+            lock (_sync)
+            {
+                foreach (var kv in _staging)
+                {
+                    var id = kv.Key;
+                    var hash = kv.Value;
+                    var st = _states[id];
+                    st.Previous = st.Current;
+                    st.Current = hash;
+                    if (hash.HasValue) _hash2Id[hash.Value] = id; else _hash2Id.Remove(st.Previous.GetValueOrDefault());
+                    _states[id] = st;
+                }
+                _staging.Clear();
+            }
+        }
+
+        public void Discard()
+        {
+            lock (_sync) { _staging.Clear(); }
+        }
+
+        public bool TryGetSnapshot(long hash, out TokenSnapshot<T> snapshot)
+        {
+            lock (_sync)
+            {
+                if (!_hash2Id.TryGetValue(hash, out var id)) { snapshot = default; return false; }
+                var st = _states[id];
+                _pool.TryGetValue(st.Initial.GetValueOrDefault(), out var iv);
+                _pool.TryGetValue(st.Previous.GetValueOrDefault(), out var pv);
+                _pool.TryGetValue(st.Current.GetValueOrDefault(), out var cv);
+                snapshot = new TokenSnapshot<T>(st.Initial, st.Previous, st.Current, iv, pv, cv);
+                return true;
+            }
+        }
+
+        public IEnumerable<TokenDiff<T>> GetCommittedDiff()
+        {
+            lock (_sync) { return BuildDiff(st => new Tuple<long?, long?>(st.Previous, st.Current)).ToList(); }
+        }
+
+        public IEnumerable<TokenDiff<T>> GetUncommittedDiff()
+        {
+            lock (_sync)
+            {
+                var diffs = new List<TokenDiff<T>>();
+                foreach (var kvp in _states)
+                {
+                    var id = kvp.Key;
+                    var st = kvp.Value;
+                    var stagedHash = _staging.TryGetValue(id, out var value) ? value : st.Current;
+                    if (stagedHash == st.Current) continue;
+                    _pool.TryGetValue(st.Current.GetValueOrDefault(), out var curVal);
+                    _pool.TryGetValue(stagedHash.GetValueOrDefault(), out var unVal);
+                    diffs.Add(new TokenDiff<T>(st.Current, stagedHash, curVal, unVal));
+                }
+                return diffs;
+            }
+        }
+
+        public IEnumerable<TokenDiff<T>> GetFullDiff()
+        {
+            lock (_sync) { return BuildDiff(st => new Tuple<long?, long?>(st.Previous ?? st.Initial, st.Current)).ToList(); }
+        }
+
+        private IEnumerable<TokenDiff<T>> BuildDiff(Func<TokenState, Tuple<long?, long?>> selector)
+        {
+            foreach (var st in _states.Values)
+            {
+                var pair = selector(st);
+                var leftHash = pair.Item1;
+                var rightHash = pair.Item2;
+                if (leftHash == rightHash) continue;
+                _pool.TryGetValue(leftHash.GetValueOrDefault(), out var leftVal);
+                _pool.TryGetValue(rightHash.GetValueOrDefault(), out var rightVal);
+                yield return new TokenDiff<T>(leftHash, rightHash, leftVal, rightVal);
+            }
+        }
+
+        private TokenOpResult StageInsert(long hash, T value)
+        {
+            if (_hash2Id.ContainsKey(hash)) return TokenOpResult.DuplicateHash;
+            var id = Guid.NewGuid();
+            _hash2Id[hash] = id;
+            _pool[hash] = value;
+            _states[id] = new TokenState();
+            _staging[id] = hash;
+            return TokenOpResult.Success;
+        }
+
+        private TokenOpResult StageDelete(long hash)
+        {
+            if (!_hash2Id.TryGetValue(hash, out var id)) return TokenOpResult.UnknownHash;
+            if (_staging.ContainsKey(id)) return TokenOpResult.AlreadyStaged;
+            _staging[id] = null;
+            return TokenOpResult.Success;
+        }
+
+        private TokenOpResult StageModify(long oldHash, long newHash, T value)
+        {
+            if (!_hash2Id.TryGetValue(oldHash, out var id)) return TokenOpResult.UnknownHash;
+            if (_hash2Id.TryGetValue(newHash, out var otherId) && otherId != id) return TokenOpResult.Collision;
+            if (_staging.ContainsKey(id)) return TokenOpResult.AlreadyStaged;
+            if (_pool.TryGetValue(newHash, out var existing) && !EqualityComparer<T>.Default.Equals(existing, value)) return TokenOpResult.Collision;
+            _hash2Id[newHash] = id;
+            _pool[newHash] = value;
+            _staging[id] = newHash;
+            return TokenOpResult.Success;
+        }
+    }
+
+    public sealed class TokenStateKeeper : ITokenInitializer<string>, ITokenMutator<string>, ITokenReader<string>
+    {
+        private sealed class CoreKeeper : StateKeeper<string> { }
+        private readonly CoreKeeper _core = new CoreKeeper();
+        public TokenOpResult Seed(long hash, string value) => _core.Seed(hash, value);
+        public TokenOpResult Stage(long? oldHash, long? newHash, string value) => _core.Stage(oldHash, newHash, value);
+        public void Commit() => _core.Commit();
+        public void Discard() => _core.Discard();
+        public bool TryGetSnapshot(long hash, out TokenSnapshot<string> snapshot) => _core.TryGetSnapshot(hash, out snapshot);
+        public IEnumerable<TokenDiff<string>> GetCommittedDiff() => _core.GetCommittedDiff();
+        public IEnumerable<TokenDiff<string>> GetUncommittedDiff() => _core.GetUncommittedDiff();
+        public IEnumerable<TokenDiff<string>> GetFullDiff() => _core.GetFullDiff();
+    }
+}
